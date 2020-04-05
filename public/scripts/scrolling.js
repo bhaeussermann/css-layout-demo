769c14a3481f8cc16795989a66ac40d70cfb0059
Bernhard Häussermann <bernhardgh@icloud.com>
2020-04-05 19:21:12 +0200

    Add independent scrolling to side-bar and main content container, and add page describing this feature

---
+++ public/scripts/scrolling.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const mainMenuList = document.getElementById('main-menu-list');
+
+  const addMenuItem = itemText => {
+    const linkElement = document.createElement('a');
+    linkElement.setAttribute('href', '#')
+    linkElement.setAttribute('class', 'pure-menu-link');
+    linkElement.appendChild(document.createTextNode(itemText));
+
+    const listElement = document.createElement('li');
+    listElement.setAttribute('class', 'pure-menu-item');
+    listElement.appendChild(linkElement);
+
+    mainMenuList.appendChild(listElement);
+  }
+
+  for (let i = 0; i < 10; i++) {
+    addMenuItem('Extra Item');
+    addMenuItem('For Scrolling');
+  }
+});
--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function(_req, res, _next) {
   res.render('index', { title: 'CSS Layout' });
 });
-router.get('/page-two', function(req, res, next) {
-  res.render('page-two', { title: 'Page Two' });
+router.get('/scrolling', function(_req, res, _next) {
+  res.render('scrolling', { title: 'Scrolling' });
 });
 
 module.exports = router;
