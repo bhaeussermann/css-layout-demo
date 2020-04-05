6e1c044948f10afbf0d598e5c82e1095982e00f0
Bernhard Häussermann <bernhardgh@icloud.com>
2020-04-05 18:25:18 +0200

    Add landing page containing a brief description of the application

--- routes/main.js
+++ routes/main.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.redirect('/page-one');
-});
-router.get('/page-one', function(req, res, next) {
-  res.render('page-one', { title: 'Page One' });
+  res.render('index', { title: 'CSS Layout' });
 });
 router.get('/page-two', function(req, res, next) {
   res.render('page-two', { title: 'Page Two' });
