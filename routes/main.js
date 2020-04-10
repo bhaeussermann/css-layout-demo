88f0b38d0e0ac05e4471584f088ba20cd0a0d36d
Bernhard Häussermann <bernhardgh@icloud.com>
2020-04-10 12:01:07 +0200

    Factor list of pages into array

--- routes/main.js
+++ routes/main.js
@@ -1,23 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-router.get('/', function(_req, res, _next) {
-  res.render('index', { title: 'CSS Layout' });
-});
-router.get('/flexbox', function(_req, res, _next) {
-  res.render('flexbox', { title: 'Flexbox' });
-});
-router.get('/grid', function(_req, res, _next) {
-  res.render('grid', { title: 'Grid' });
-});
-router.get('/media-queries', function(_req, res, _next) {
-  res.render('media-queries', { title: 'Media Queries' });
-});
-router.get('/scrolling', function(_req, res, _next) {
-  res.render('scrolling', { title: 'Scrolling' });
-});
-router.get('/tabs', function(_req, res, _next) {
-  res.render('tabs', { title: 'Tabs' });
-});
+const pages = [
+  {
+    route: '/',
+    name: 'index',
+    title: 'CSS Layout',
+    sideBarText: 'Home'
+  },
+  { name: 'flexbox', title: 'Flexbox' },
+  { name: 'grid', title: 'Grid' },
+  { name: 'media-queries', title: 'Media queries' },
+  { name: 'scrolling', title: 'Scrolling' },
+  { name: 'tabs', title: 'Tabs' }
+];
+
+for (const page of pages) {
+  page.route = page.route || '/' + page.name;
+  page.sideBarText = page.sideBarText || page.title;
+}
+
+for (const page of pages) {
+  router.get(page.route, function(_req, res, _next) {
+    res.render(page.name, { title: page.title, pages: pages });
+  });
+}
 
 module.exports = router;
